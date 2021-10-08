068343654dd2baf5846670bd722951c6a07df817
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-08 15:22:36 +0200

    inserimento commento

--- main.js
+++ main.js
@@ -11,5 +11,7 @@ const userLastName = prompt("qual'è il tuo cognome");
 const userColor = prompt("qual'è il tuo colore preferito?");
 
 
+// passwprd
+
 document.getElementById('password').innerHTML = userName + userLastName + userColor + 21;
 
