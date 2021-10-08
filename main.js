a880f8405a0af817b0b10be580d0d725ab041db3
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-08 15:21:41 +0200

    insermento domande

---
+++ main.js
@@ -0,0 +1,15 @@
+// chiedi all'utente il suo nome 
+
+const userName = prompt("Come ti chiami?");
+
+// chiedi il suo cognome
+
+const userLastName = prompt("qual'è il tuo cognome");
+
+// chiedi il suo colore preferito
+
+const userColor = prompt("qual'è il tuo colore preferito?");
+
+
+
+
